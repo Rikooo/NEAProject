44ed5a2556cc575d792979e08b65f7b8e1f7d1b8
Rikooo <58711336+Rikooo@users.noreply.github.com>
2020-06-23 11:37:55 +0100

    Add files via upload

---
+++ NEA Project/DoNotCrash_v1/button.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class Button:
+    """ Class Generates a button with text """
+
+    def __init__(self, text, text_colour, text_size, x, y, width, height):
+        self.text = text
+        self.text_colour = text_colour
+        self.text_size = text_size
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, GAME_DISPLAY):
+        # Need a smaller font to fit into the button
+        font = pygame.font.Font(
+            'NEA Project\external files\street.ttf', self.text_size)
+        # The actual rectangle 'hitbox' for the button (Transparrent To The User)
+        pygame.draw.rect(GAME_DISPLAY, (0, 0, 0),
+                         (self.x, self.y, self.width, self.height), -1)  # -1 sets the width to a negative number thus removing all background and borders from the user
+        text_to_button = font.render(self.text, True, self.text_colour)
+
+        # Centres Text by using pygame functions
+        GAME_DISPLAY.blit(text_to_button, (self.x + (self.width/2 - text_to_button.get_width()/2),
+                                           self.y + (self.height/2 - text_to_button.get_height()/2)))
+
+    def mouseOver(self, pos):
+        # Detects collision with mouse and rectangle
+        if self.x + self.width > pos[0] > self.x and self.y + self.height > pos[1] > self.y:
+            return True
+        return False
---
+++ NEA Project/DoNotCrash_v1/misc.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+# Colours  ------------------------------------------------------------------#
+ORANGE = (255, 182, 0)
+NEON_GREEN = (83, 212, 137)
+WHITE = (240, 240, 240)
+GREY = (192, 192, 192)
+TRANSPARENT = (0, 0, 0, 0)
+
+# Images  ------------------------------------------------------------------------------#
+# 1280x720
+main_menu_image = pygame.image.load(
+    'NEA Project\external files\main_menu_image.jpg')
+# 1280x720
+choosing_menu_image = pygame.image.load(
+    'NEA Project\external files\choosing_menu_image.jpg')
+# 32x32
+game_icon_image = pygame.image.load(
+    'NEA Project\external files\game_icon_image.png')
+# 64x64
+arrow_left_image = pygame.image.load(
+    'NEA Project/external files/arrow_left_image.png')
+# 64x64
+arrow_right_image = pygame.image.load(
+    'NEA Project/external files/arrow_right_image.png')
